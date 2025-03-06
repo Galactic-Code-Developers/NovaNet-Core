2a44317aa78e053e738d53992254aac86791952c
Antonis Valamontes <tony@geopay.me>
2025-03-07 00:28:50 +0200

    Create App.js

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ValidatorTable from "./ValidatorTable";
+
+const App = () => {
+    return (
+        <div>
+            <h1>🚀 NovaNet AI Validator Dashboard</h1>
+            <ValidatorTable />
+        </div>
+    );
+};
+
+export default App;
